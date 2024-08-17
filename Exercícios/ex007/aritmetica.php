8c4bc4a4662069427a89ae203ed605b5187c3fa2
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-08-16 21:51:19 -0300

    Operadores aritméticos no PHP

---
+++ Exercícios/ex007/aritmetica.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Expressões Aritméticas</title>
+</head>
+
+<body>
+  <h1>Expressões aritméticas</h1>
+
+  <?php 
+  $res = 50 / (2 + 3) ** 2;
+    echo "O resultado de 5 + 2 / 2, é igual a $res";
+  ?>
+</body>
+
+</html>
