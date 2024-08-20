cdaaece436fd4a8f37eb9ac917f4ae4d1d969165
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-08-19 23:57:42 -0300

    Separando os do curso módulos em pastas + Pequenas mudanças no desafio 1 de PHP

--- Módulo 1/Desafios/1. Antecessor e Sucessor/numRet.php
+++ Módulo 1/Desafios/1. Antecessor e Sucessor/numRet.php
@@ -3,8 +3,9 @@
 
 <head>
   <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Resultado</title>
+  <title>Antecessor e Sucessor | Resultado</title>
   <link rel="stylesheet" href="style.css">
 </head>
 
@@ -13,23 +14,23 @@
     <h1>Resultado</h1>
 
     <?php 
-      $num = $_GET["num"];
+      $num = $_GET["num"] ?? 0;
 
       echo "<p>O número digitado foi <strong>$num</strong></p>";
     ?>
 
     <?php 
       $antecessor = $num - 1;
-      echo "<p>O seu antecessor é <strong>$antecessor</strong></p>";
+      echo "<p>O seu <em>antecessor</em> é <strong>$antecessor</strong></p>";
     ?>
 
     <?php 
       $sucessor = $num + 1;
-      echo "<p>O seu sucessor é <strong>$sucessor</strong></p>";
+      echo "<p>O seu <em>sucessor</em> é <strong>$sucessor</strong></p>";
     ?>
 
     <button class="linkButt">
-      <a href="javascript:history.go(-1)" target="_self" rel="prev">Voltar</a>
+      <a href="javascript:history.go(-1)" target="_self" rel="prev">&#x2B05; Voltar</a>
     </button>
   </section>
 
