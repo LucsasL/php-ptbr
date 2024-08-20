5b25b73cf31626efb928b4afb67210495489c871
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-08-20 12:23:38 -0300

    Desafio do conversor de moedas usando biblioteca de internacionalização (INTL)

--- Módulo 1/Desafios/3. Conversor de Moedas/convertCurrency.php
+++ Módulo 1/Desafios/3. Conversor de Moedas/convertCurrency.php
@@ -3,6 +3,7 @@
 
 <head>
   <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Conversor de Moedas | Resultado</title>
   <link rel="stylesheet" href="style.css">
@@ -16,9 +17,17 @@
       </h1>
 
       <?php 
-        $dinReal = $_GET["dinheiro"];
+        $dinReal = $_GET["dinheiro"] ?? 0;
         $dinDolar = $dinReal / 5.47;
-        echo "<p>Seus R$$dinReal equivalem a <strong>US\$$dinDolar</strong></p>";
+        // echo "<p>Seus R$" . number_format($dinReal, 2, ",", ".") . " equivalem a <strong>US$" . number_format($dinDolar, 2, ",", ".") . "</strong></p>";
+
+        // Formatação de moedas com internacionalização
+
+        // Internacionalizaçãp usando a biblioteca intl (Internalization)
+        
+        $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
+        
+        echo "<p>Seus " . numfmt_format_currency($padrao,$dinReal, "BRL") . " equivalem a <strong>" . numfmt_format_currency($padrao, $dinDolar, "USD") . "</strong></p>";
       ?>
 
       <p>
