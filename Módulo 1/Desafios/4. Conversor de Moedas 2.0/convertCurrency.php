88f5145eaed4c3ae9efc977c8f1b0ebcb4cc43b9
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-08-21 05:08:21 -0300

    Finalizando o Conversor de Moedas com integração da API do Banco Central

--- Módulo 1/Desafios/4. Conversor de Moedas 2.0/convertCurrency.php
+++ Módulo 1/Desafios/4. Conversor de Moedas 2.0/convertCurrency.php
@@ -3,6 +3,7 @@
 
 <head>
   <meta charset="UTF-8">
+  <meta http-equiv="X-UA_Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Conversor de Moedas 2.0 | Resultado</title>
   <link rel="stylesheet" href="style.css">
@@ -15,15 +16,32 @@
         Conversor de moedas v2.0
       </h1>
 
-      <!-- Usar API para fazer captura da cotação do dólar -->
-      <?php 
-        $dinReal = $_GET["dinheiro"];
-        // $dinDolar = $;
-        echo "<p>Seus R$$dinReal equivalem a <strong>US\$$dinDolar</strong></p>";
+      <?php
+        // Pegando a data do Sistema Operacional
+        $inicio = date("m-d-Y", strtotime("-7 days"));
+        $atual = date("m-d-Y");
+
+        // Puxando dados de uma API e tratando como JSON
+        $url = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=%27$inicio%27&@dataFinalCotacao=%27$atual%27&\$top=1&\$orderby=dataHoraCotacao%20desc&\$format=json&\$select=cotacaoCompra,cotacaoVenda,dataHoraCotacao";
+        $dados = json_decode(file_get_contents($url), true);
+
+        // Puxando input de usuário
+        $dinReal = $_GET["dinheiro"] ?? 0;
+        
+        // Definindo um padrão de formatação de moeda
+        $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
+        
+        $cotacaoDolar = $dados["value"][0]["cotacaoCompra"];
+        $dinDolar = $dinReal / $cotacaoDolar;
+        
+        echo "<p>Seus " . numfmt_format_currency($padrao, $dinReal, "BRL") . " equivalem a <strong>" . numfmt_format_currency($padrao, $dinDolar, "BRL") . "</strong></p>";
       ?>
 
       <p>
-        *Cotação obtida diretamente do site do <strong>Banco Central do Brasil</strong>
+        *Cotação obtida diretamente do site do
+        <a href="https://bcb.gov.br" target="_blank" rel="noopener norefferer">
+          Banco Central do Brasil
+        </a>
       </p>
 
       <button class="linkButt">
