f0bf9bacaa71838f0fbdbeebd09bc50f3f25bdba
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-08-21 05:53:32 -0300

    Refatorando código do Analisador de Números Reais

--- Módulo 1/Desafios/5. Analisador de Número Real/analiseNum.php
+++ Módulo 1/Desafios/5. Analisador de Número Real/analiseNum.php
@@ -3,6 +3,7 @@
 
 <head>
   <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Analisador de Número Real | Resultado</title>
   <link rel="stylesheet" href="style.css">
@@ -16,20 +17,19 @@
       </h1>
 
       <?php 
-        $numReal = $_GET["numReal"];
-        $numInt = round($numReal);
-        $numDec = $numReal - $numInt;
+        $numReal = $_POST["numReal"];
         
-        echo "<p>Analisando o número <strong>$numReal</strong> informado pelo usuário:</p>";
+        $numInt = (int) $numReal;
+        $numFrac = $numReal - $numInt;
+        
+        echo "<p>Analisando o número <strong>" . number_format($numReal, 3, ",", ".") . "</strong> informado pelo usuário:</p>";
       ?>
 
       <ul>
         <?php
-          echo "<li>A parte inteira do número é <strong>$numInt</strong></li>";
-        ?>
+          echo "<li>A parte inteira do número é <strong>" . number_format($numInt, 0, ",", ".") . "</strong></li>";
 
-        <?php 
-          echo "<li>A parte fracionária do número é <strong>$numDec</strong></li>";
+          echo "<li>A parte fracionária do número é <strong>" . number_format($numFrac, 3, ",", ".") . "</strong></li>";
         ?>
       </ul>
     </main>
