d0cb17a61aeee69a26ff3356e4b2dcba7542752e
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-09-08 23:53:25 -0300

    Iniciando curso de PHP antigo

---
+++ PHP (2013)/Exercícios/1. Olá/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <title>Primeiro exercício</title>
+
+  <style>
+  h2 {
+    color: red;
+    text-shadow: 1px 1px 0px black;
+  }
+  </style>
+</head>
+
+<body>
+  <h1>
+    Primeiro exercício
+  </h1>
+
+  <?php 
+    echo "<h2>Olá, Mundo!</h2>";
+  ?>
+</body>
+
+</html>
