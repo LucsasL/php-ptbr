5e972a7f9b63970cae40ba176664d363753349fe
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-09-12 21:33:37 -0300

    Exercício de Processamento de Dados Concluído

---
+++ PHP (2013)/Exercícios/17. Processando Dados Pessoais/Dados Pessoais.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <title>Coletando Dados Pessoais</title>
+  <link rel="stylesheet" href="_css/estilo.css" />
+</head>
+
+<body>
+
+  <div class="formulario">
+    <h2>
+      Coletando Dados Pessoais
+    </h2>
+
+    <?php 
+      $nome = $_REQUEST["nome"] ?? "Desconhecido";
+      $anoNasc = $_REQUEST["anoNasc"] ?? 0;
+      $sexo = $_REQUEST["sexo"] ?? "Não Especificado";
+      $data = date("Y");
+      $idade = $data - $anoNasc;
+    ?>
+
+    <form action="<?= $_SERVER["PHP_SELF"]?>" method="get" autocapitalize="on">
+      <label for="nome">Nome: </label> <br />
+      <input type="text" name="nome" id="nomeID" value="<?= $nome ?>" required /> <br />
+
+      <label for="anoNasc">Ano Nascimento: </label> <br />
+      <input type="number" name="anoNasc" id="anoNascID" value="<?= $anoNasc ?>" min="1910" max="<?= $data ?>"
+        required />
+      <br />
+
+      <label for="sexo">Sexo: </label> <br />
+      <input type="radio" name="sexo" id="masc" value="Masculino" checked />
+      <label for="masc">Masculino</label> <br />
+
+      <input type="radio" name="sexo" id="fem" value="Feminino" />
+      <label for="fem">Feminino</label> <br />
+
+      <input type="submit" value="Mostrar dados" />
+    </form>
+
+    <?php 
+      echo "<p>O nome é $nome</p>";
+      echo "<p>A Idade é $idade</p>";
+      echo "<p>O sexo é $sexo</p>";
+    ?>
+  </div>
+
+</body>
+
+</html>
