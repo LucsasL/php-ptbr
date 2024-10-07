2680c8488a12dec5bfa73bfceae8aac2e354f29d
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-10-07 20:45:10 -0300

    Exercício Contagem com While Loop

---
+++ PHP (2013)/Exercícios/24. Contagem com While Loop/Operações com While Loop.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <title>Contagem Progressiva</title>
+  <link rel="stylesheet" href="_css/estilo.css" />
+</head>
+
+<body>
+
+  <div class="formulario">
+    <h2>
+      Contagem Progressiva
+    </h2>
+
+    <?php 
+      // Contagem Progressiva
+      $c = 1;
+      
+      while ($c <= 10) {
+        echo "<p>$c</p>";
+        $c++;
+      }
+      
+      // Contagem Regressiva
+      $c = 10;
+      
+      while ($c >= 1) {
+        echo "<p>$c</p>";
+        $c--;
+      }
+      
+      // Contagem Regressiva de dois em dois
+      $c = 10;
+      
+      while ($c >= 0) {
+        echo "<p>$c</p>";
+        $c -= 2;
+      }
+      
+    ?>
+  </div>
+
+</body>
+
+</html>
