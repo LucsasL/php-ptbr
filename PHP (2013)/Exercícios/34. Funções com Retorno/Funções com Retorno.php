e733de93f90301158f7a3e7caabf1bc2e5d9a704
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-10-10 18:46:52 -0300

    Exercício de Funções com Retorno

--- PHP (2013)/Exercícios/34. Funções com Retorno/Funções com Retorno.php
+++ PHP (2013)/Exercícios/34. Funções com Retorno/Funções com Retorno.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <title>Funções em PHP</title>
+  <title>Funções com Retorno</title>
   <link rel="stylesheet" href="_css/estilo.css" />
 </head>
 
@@ -13,21 +13,19 @@
 
   <div class="formulario">
     <h2>
-      Funções em PHP
+      Funções com Retorno
     </h2>
 
     <?php 
       function soma($num1, $num2) {
-        echo "<p>A soma de $num1 e $num2 é " . $num1 + $num2 . "</p>";
+        return $num1 + $num2;
       }
-
-      soma(5, 2);
-      soma(7, 4);
-      soma(15, 84);
       
-      $x = 9;
-      $y = 18;
-      soma($x, $y);
+      $x = 5;
+      $y = 7;
+      $res = soma($x, $y);
+      
+      echo "<p>A soma de $x e $y é " . $res . "</p>";
     ?>
 
   </div>
